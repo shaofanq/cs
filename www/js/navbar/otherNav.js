e6f51fba934e3bdfd8705c8672a536183993eabe
Spencer Wise <spencerrwise@gmail.com>
2015-02-12 23:21:55 -0700

    fuck you spencer

---
+++ www/js/navbar/otherNav.js
@@ -0,0 +1,9 @@
+var app = angular.module('cs');
+
+app.directive('othernav', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '../templates/other-nav.html',
+        controller: 'NavbarController'
+    }
+});
